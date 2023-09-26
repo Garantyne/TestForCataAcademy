01e95307ce2212457282654519e34df622a95d4d
garantyne <krel.andrej@yandex.ru>
2023-09-26 11:34:52 +0700

    12345

--- main.go
+++ main.go
@@ -80,41 +80,60 @@ func decipheringRomanNumerals(arifmet string) (int, int) {
 	str2 := arifmet[signIndex+1:]
 	str2 = strings.TrimSpace(str2)
 
-	num1 := parseDigit(str1)
-	num2 := parseDigit(str2)
+	num1, isRomeSystem1, isArabSystem1 := parseDigit(str1)
+	num2, isRomeSystem2, isArabSystem2 := parseDigit(str2)
+	if isRomeSystem1 && isArabSystem2 || isRomeSystem2 && isArabSystem1 {
+		panic("Разные системы счисления, аварийное завершение")
+	}
+	if (isRomeSystem2 && isRomeSystem1) && num1 < num2 {
+		panic("Аварийное завершение в римской системе небыло отрицательных чисел")
+	}
 
 	return num1, num2
 }
 
-func parseDigit(digit string) int {
+func parseDigit(digit string) (int, bool, bool) {
 	var num1 int
+	var isRomeSystem bool = false
+	var isArabSystem bool = false
 	if digit == "I" {
 		num1 = 1
+		isRomeSystem = true
 	} else if digit == "II" {
 		num1 = 2
+		isRomeSystem = true
 	} else if digit == "III" {
 		num1 = 3
+		isRomeSystem = true
 	} else if digit == "IV" {
 		num1 = 4
+		isRomeSystem = true
 	} else if digit == "V" {
 		num1 = 5
+		isRomeSystem = true
 	} else if digit == "VI" {
 		num1 = 6
+		isRomeSystem = true
 	} else if digit == "VII" {
 		num1 = 7
+		isRomeSystem = true
 	} else if digit == "VIII" {
 		num1 = 8
+		isRomeSystem = true
 	} else if digit == "IX" {
 		num1 = 9
+		isRomeSystem = true
 	} else if digit == "X" {
 		num1 = 10
+		isRomeSystem = true
 	} else {
 		var err error
 		num1, err = strconv.Atoi(digit)
+		isArabSystem = true
 		if err != nil {
 			fmt.Println("Ошибка преобразования строки в число:", err)
 			panic("Ошибка")
 		}
 	}
-	return num1
+	return num1, isRomeSystem, isArabSystem
 }
